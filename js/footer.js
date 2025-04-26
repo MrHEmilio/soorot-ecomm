535006788090de3a7c8cf571c927a2f866ae5384
Fernandobm <fbartolomem.98@gmail.com>
2025-04-26 17:38:14 -0600

    agregando el footer con JS

--- js/footer.js
+++ js/footer.js
@@ -1,15 +1,54 @@
-//Cargar el archivo HTML
-fetch('footer.html')
-    .then(response => response.text("Exitoso"))
-    .then(html => {
-        //Crear un elemento temporal para contener el HTML cargado 
-        var temporal = document.createElement('footer');
-        temporal.innerHTML = html;
-
-        //Obtenemos el elemento nav
-        var nav = temporal.querySelector('footer');
-
-        //agregamos al documento
-        var contenedor = document.getElementById('contenedor');
-        contenedor.insertBefore(nav, contenedor.firstChild);
-    });
+export function insertarfooter() {
+    const footer = document.createElement("footer");
+    footer.innerHTML = `
+        <div class="footer-container">
+        <div class="footer-column">
+          <img src="./assets/logo-soorot.svg" alt="" />
+        </div>
+        <div class="footer-column">
+          <h3>Soorot</h3>
+          <ul>
+            <li><a href="#">Cerca</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Contactanos</a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#">Historia</a></li>
+          </ul>
+        </div>
+        <div class="footer-column">
+          <form>
+            <label for="email">Ingresa tu correo</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Ingresa tu correo..."
+            />
+            <button type="submit">Enviar</button>
+            <p></p>
+          </form>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <p>© 2025 Soorot. All rights reserved.</p>
+        <ul>
+          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#">Terms of Service</a></li>
+          <li><a href="#">Cookies Settings</a></li>
+        </ul>
+        <div class="social-media">
+          <a href="http://www.github.com/"
+            target="_blank"
+            class="icon-github"></a>
+          <a  href="http://www.instagram.com/"
+            target="_blank"
+            class="icon-instagram"></a>
+          <a href="http://www.facebook.com/"
+            target="_blank"
+            class="icon-facebook2"></a>
+          <a href="./contactanos.html" target="_blank" class="icon-bubble"></a>
+        </div>
+      </div>
+    `;
+    document.body.appendChild(footer);
+}
